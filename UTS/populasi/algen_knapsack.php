6c590fd52200f3c1ff11decde5a07264c226b325
Aldi #ARPATEAM <arpateam.com@gmail.com>
2022-01-11 08:53:37 +0700

    Perbaikan

---
+++ UTS/populasi/algen_knapsack.php
@@ -0,0 +1,56 @@
+<?php
+
+	class Catalogue{
+		function createProductColumn($columns, $listOfRawProduct){
+			foreach (array_keys($listOfRawProduct) as $listOfRawProductKey) {
+				$listOfRawProduct[$columns[$listOfRawProductKey]] = $listOfRawProduct[$listOfRawProductKey];
+				unset($listOfRawProduct[$listOfRawProductKey]);
+			}
+			return $listOfRawProduct;
+		}
+
+		function product($parameters){
+			$collectionOfListProduct = [];
+
+			$raw_data = file($parameters['file_name']);
+
+			foreach ($raw_data as $listOfRawProduct) {
+				$collectionOfListProduct[] = $this->createProductColumn($parameters['columns'], explode(",", $listOfRawProduct));
+			}
+
+			return [
+				'product' => $collectionOfListProduct,
+				'gen_length' => count($collectionOfListProduct)
+			];
+		}
+	}
+
+	class PopulationGenerator{
+		function createIndividu($parameters){
+			$catalogue = new Catalogue;
+			$lengthOfGen = $catalogue->product($parameters)['gen_length'];
+			for ($i=0; $i <= $lengthOfGen-1 ; $i++) { 
+				$ret[] = rand(0, 1);
+			}
+			return $ret;
+		}
+
+		function createPopulation($parameters){
+			for ($i=0; $i <= $parameters['population_size']; $i++) { 
+				$ret[] = $this->createIndividu($parameters);
+			}
+			print_r($ret);
+		}
+	}
+
+	$parameters = [
+		'file_name' => '../product.txt',
+		'columns' => ['items', 'price'],
+		'population_size' => 4 
+	];
+
+	$katalog = new Catalogue;
+	$katalog->product($parameters);
+
+	$initialPopulation = new PopulationGenerator;
+	$initialPopulation->createPopulation($parameters);
